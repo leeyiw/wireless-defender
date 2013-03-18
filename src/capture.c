8596c276e073fdc9dc352537094dba8b72d838ea
liyiwei <leeyiw@gmail.com>
2013-03-18 22:07:59 +0800

    add capture module function

--- src/capture.c
+++ src/capture.c
@@ -2,13 +2,53 @@
 
 static pcap_t *device;
 static char errbuf[PCAP_ERRBUF_SIZE];
+static pcap_handler capture_callback;
+static int capture_cnt;
+static u_char *capture_callback_arg;
 
 void
-WD_capture_init()
+WD_capture_init(pcap_handler callback, int cnt, u_char *callback_arg);
 {
 	device = pcap_open_live(g_interface, 65535, 0, 0, errbuf);
 	if(device == NULL) {
 		user_exit1("open device '%s' for capturing error: %s",
 			g_interface, errbuf);
+	}
+	capture_callback = callback;
+	capture_cnt = cnt;
+	capture_callback_arg = callback_arg;
+}
+
+void
+WD_capture_set_callback(pcap_handler callback)
+{
+	capture_callback = callback;
+}
+
+void
+WD_capture_set_cnt(int cnt)
+{
+	capture_cnt = cnt;
+}
+
+void
+WD_capture_set_callback_arg(u_char *callback_arg)
+{
+	capture_callback_arg = callback_arg;
+}
+
+void
+WD_capture_start()
+{
+	int ret;
+
+	ret = pcap_loop(device, capture_cnt, capture_callback,
+		capture_callback_arg);
+	if(ret == 0) {
+		user_info("capture count exhausted");
+	} else if(ret == -2) {
+		user_info("capture was breaked");
+	} else if(ret == -1) {
+		pcap_perror(device, "capture error");
 	}
 }
--- src/capture.h
+++ src/capture.h
@@ -5,6 +5,11 @@
 
 #include "config.h"
 
-extern void WD_capture_init();
+extern void WD_capture_init(pcap_handler callback, int cnt,
+	u_char *callback_arg);
+extern void WD_capture_set_callback(pcap_handler callback);
+extern void WD_capture_set_cnt(int cnt);
+extern void WD_capture_set_callback_arg(u_char *callback_arg);
+extern void WD_capture_start();
 
 #endif
