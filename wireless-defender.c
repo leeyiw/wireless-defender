3010dfc8d8b4e72b3c3b558b0f00396fdd32d186
liyiwei <leeyiw@gmail.com>
2013-04-18 22:30:26 +0800

    fix wireless-defender.c conflict

--- wireless-defender.c
+++ wireless-defender.c
@@ -39,8 +39,8 @@ main(int argc, char *argv[])
 {
 	WD_init();
 
-	//server_pid = fork();
-	//if(server_pid == 0) {
+	server_pid = fork();
+	if(server_pid == 0) {
 		// 子进程，监听并处理客户端连接请求
 
 		// 初始化服务器模块
@@ -49,28 +49,25 @@ main(int argc, char *argv[])
 		WD_server_main_loop();
 
 		return EXIT_SUCCESS;
-	//} else if(server_pid != -1) {
-	//	// 父进程，进行抓包
+	} else if(server_pid != -1) {
+		// 父进程，进行抓包
 
-	//	// 初始化抓包模块
-	//WD_capture_init(WD_analyse_test, 1, (u_char *)1);
-	//	// 启动抓包
-<<<<<<< HEAD
-	WD_capture_start();
+		// 初始化抓包模块
+		WD_capture_init(WD_analyse_test, 1, (u_char *)1);
+		// 启动抓包
+		WD_capture_start();
 
-=======
-	//WD_capture_start();
->>>>>>> 1b3d54a055fbbd635242fec77e3577e303670ad1
-	//	// 关闭抓包模块
-	//WD_capture_destory();
-	//	// 清理抓包模块
-	//	WD_destory();
+		WD_capture_start();
+		// 关闭抓包模块
+		WD_capture_destory();
+		// 清理抓包模块
+		WD_destory();
 
-	//	return EXIT_SUCCESS;
-	//} else {
-	//	// 异常情况
-	//	err_exit("create process error");
-	//}
+		return EXIT_SUCCESS;
+	} else {
+		// 异常情况
+		err_exit("create process error");
+	}
 	
 	return EXIT_SUCCESS;
 }
