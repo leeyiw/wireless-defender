51822883938faf07b8daf02de6b6117c474fb637
faye <fayewu37@gmail.com>
2013-06-07 16:11:08 +0800

    rework

--- wireless-defender.c
+++ wireless-defender.c
@@ -1,15 +1,19 @@
 #include <time.h>
 
+#include "wireless-defender.h"
 #include "config.h"
 #include "utils.h"
 #include "capture.h"
-#include "analyse.h"
 #include "server.h"
 #include "utils.h"
 #include "analyse.h"
+#include "preprocess.h"
 
 /* 起始运行时间 */
 time_t WD_start_time;
+AP_list_t *AP_list = NULL;
+queue_t *q = NULL;
+u_char ssid[105] = { 0x5c, 0x63, 0xbf, 0x37, 0x34, 0xea };
 
 /**
  * 主程序全局初始化函数
@@ -23,6 +27,22 @@ WD_init()
 	}
 	// 初始化配置文件模块
 	WD_config_init();
+	analyse_init();
+}
+
+void
+analyse_init()
+{
+	q = ( queue_t *)malloc( sizeof( q ) );	
+	q->head = 1;
+	q->tail = 0;
+
+	AP_list = ( AP_list_t * )malloc( sizeof( AP_list_t ) );
+	AP_list->head = NULL;
+	AP_list->tail = NULL;
+	AP_list->cur = NULL;
+
+	pthread_mutex_init( &AP_list->lock, NULL );
 }
 
 /**
@@ -36,13 +56,15 @@ WD_destory()
 int
 main(int argc, char *argv[])
 {
-	WD_init();
 
+	WD_init();
 	// 初始化服务器模块
 	//WD_server_init();
 
 	// 初始化抓包模块
-	WD_capture_init(WD_analyse_test, 1, (u_char *)1);
+	WD_capture_init(WD_analyse_test, 0, (u_char *)1);
+	//启动预处理模块
+	WD_pipe_create(&prepline);	
 	// 启动抓包
 	WD_capture_start();
 	// 关闭抓包模块
@@ -50,5 +72,6 @@ main(int argc, char *argv[])
 	// 清理抓包模块
 	WD_destory();
 
+	pthread_exit( NULL );
 	return EXIT_SUCCESS;
 }
--- wireless-defender.h
+++ wireless-defender.h
@@ -3,4 +3,5 @@
 
 extern time_t WD_start_time;
 
+extern void analyse_init(); 
 #endif
